2bfa950092f345aa081c0d9f14fdf2a38ad906f5
sonendra.singh <sonendra@niralnetworks.in>
2025-03-09 12:58:31 +0530

    backend issue fixed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,20 +17,6 @@ mongoose
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
-
-// Check for production environment
-if (process.env.NODE_ENV === "production") {
-  console.log("Running in production mode");
-
-  const __dirname1 = path.resolve();
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, "/frontend/build/index.html"));
-  });
-} else {
-  console.log("Running in development mode");
-  // You can add development-specific middleware or routes here
-}
 
 // Routes
 const destinationRoutes = require("./routes/destinations");
